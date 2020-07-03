7be8cd7673b6aa10c9c1f7838ff25ec4fc1cc925
Nguyen Luc <lucnv1@vnext.com.vn>
2020-07-03 10:54:37 +0900

    add sort function

--- src/main/java/com/sample/controller/TaskController.java
+++ src/main/java/com/sample/controller/TaskController.java
@@ -1,7 +1,13 @@
 package com.sample.controller;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -10,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.comparator.Comparators;
 import org.springframework.web.bind.annotation.*;
 
 import com.sample.model.Task;
@@ -28,23 +35,57 @@ public class TaskController {
 	public String index(Model model) {
 	    List<Task> listTasks = service.listALlTask();
 	    List<Task> listComplete = new ArrayList<>();
-	    List<Task> listNotComplete = new ArrayList<>();
+	    List<Task> listUnComplete = new ArrayList<>();
 	    
 	    for (Task task : listTasks) {
-	    	
-			if (task.getComplete_flg() == 1) {
-				listComplete.add(task);
-			} else {
-				listNotComplete.add(task);
-			}
+	    	if (task.getDelete_flg() == 0) {
+	    		if (task.getComplete_flg() == 1 ) {
+					
+					listComplete.add(task);
+				} else {
+					listUnComplete.add(task);
+				}
+	    	}
 		}
-	    
 	   
-	    
+		Collections.sort(listComplete, new Comparator<Task>() {
+            @Override
+            public int compare(Task task, Task task2) {
+                return task2.getComplete_date().compareTo(task.getComplete_date());
+            }
+        });
+		
+		Collections.sort(listUnComplete, new Comparator<Task>() {
+            @Override
+            public int compare(Task task, Task task2) {
+            	Date currentDate = new Date();
+            	Date date1 = null;
+            	Date date2 = null;
+            	SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+				try {
+					date1 = sdf.parse(task.getScheduled_date());
+					date2 = sdf.parse(task2.getScheduled_date());
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            	if ((date1.getTime() - currentDate.getTime()) > (date2.getTime() - currentDate.getTime())) {
+            		return 1;
+            	} else  if ((date1.getTime() - currentDate.getTime()) < (date2.getTime() - currentDate.getTime())) {
+            		return -1;
+            	} else {
+            		return 0;
+            	}
+                
+            }
+        });
+	
 	    model.addAttribute("listTasksComplete", listComplete);
-	    model.addAttribute("listTasksNotComplete", listNotComplete); 
+	    model.addAttribute("listTasksUnComplete", listUnComplete); 
 	    return "tasks/index";
 	}
+	
+	
 	
 	@GetMapping("new")
     public String newTask(Model model) {
@@ -79,10 +120,14 @@ public class TaskController {
 
     @PostMapping("update/{id}")
     public String update(@PathVariable int id, @ModelAttribute Task task) {
-    	
-        task.setId(id);
+    	Task taskUpdate = service.get(id); 
+    	taskUpdate.setTitle(task.getTitle());
+    	taskUpdate.setDescription(task.getDescription());
+    	taskUpdate.setComplete_date(task.getComplete_date());
+    	taskUpdate.setScheduled_date(task.getScheduled_date());
+    	taskUpdate.setPriority(task.getPriority());
         task.setUpdated_at(sdf.format(new Date()));
-        service.save(task);
+        service.save(taskUpdate);
         return "redirect:/tasks";
     }
     
@@ -95,13 +140,18 @@ public class TaskController {
         return "redirect:/tasks";
     }
 
-    @PostMapping("delete/{id}")
-    public String destroy(@PathVariable int id) {
-        Task task = service.get(id);
-        task.setId(id);
-        task.setUpdated_at(new Date().toString());
-		task.setDelete_flg(1);
-        service.save(task);
+    @PostMapping("delete/{listId}")
+    public String destroy(@PathVariable String listId) {
+    	String[] parts = listId.split(",");
+    	for (String id : parts) {
+    		
+    		Task task = service.get(Integer.decode(id));
+           
+            task.setUpdated_at(sdf.format(new Date()));
+    		task.setDelete_flg(1);
+            service.save(task);
+		}
+        
         return "redirect:/tasks";
     }
 }
